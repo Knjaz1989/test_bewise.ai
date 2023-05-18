ce12741d410fc20aed5d724288384758ca9302d6
Igor Sverchkov <knjaz1989@gmail.com>
2023-05-18 16:24:17 +0300

    create user works

--- task_2/api/database/base_handlers.py
+++ task_2/api/database/base_handlers.py
@@ -8,11 +8,11 @@ from database.models import User
 
 async def add_user(session: AsyncSession, name: str):
     insert_stm = insert(User).values(
-        name=name, uuid=uuid4
+        name=name, uuid=uuid4()
     )
     insert_data = await session.execute(
         insert_stm.returning(User.id, User.uuid)
     )
     await session.commit()
-    insert_data = insert_data.scalars().one()
+    insert_data = insert_data.mappings().one()
     return insert_data
